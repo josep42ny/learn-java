840048998bccf247f2f429eba67decfa1476dc12
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-09-25 12:26:16 +0200

    feat: add work hours to Developer

--- tasca/src/main/java/josep42ny/Developer.java
+++ tasca/src/main/java/josep42ny/Developer.java
@@ -3,9 +3,18 @@ package josep42ny;
 public class Developer {
 
     private String name;
+    private int workHours;
 
     public Developer(String name) {
         this.name = name;
+    }
+
+    public void setWorkHours(int workHours) {
+        this.workHours = workHours;
+    }
+
+    public int getWorkHours() {
+        return workHours;
     }
 
     public String getName() {
